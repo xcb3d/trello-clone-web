2f7f9f9ba133f3c5cf7abb24c7e58a9a42de495f
xcb3d <xcode3d@gmail.com>
2024-11-03 11:00:17 +0700

    Config build mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
+let apiRoot = ''
+console.log('process.env.BUILD_MODE', process.env)
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-8qng.onrender.com'
+}
 // export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-8qng.onrender.com'
+export const API_ROOT = apiRoot
